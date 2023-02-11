6f96f9235353a6b13d8536dee940def5a7575dbd
Nguyễn Việt Kim <114214103+KGC31@users.noreply.github.com>
2023-02-11 16:46:52 +0700

    Add files via upload

---
+++ Javascript/Portfolio website/Scroll to top.js
@@ -0,0 +1,8 @@
+//Force window to scroll to top of the page when refresh
+if (history.scrollRestoration) {
+    history.scrollRestoration = 'manual';
+} else {
+    window.onbeforeunload = function () {
+        window.scrollTo(0, 0);
+    }
+}
